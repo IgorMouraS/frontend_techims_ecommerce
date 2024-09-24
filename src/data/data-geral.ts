7efebb0897d23b7ed007e552a472e26a31b259a1
IgorMouraS <igormsousa2003@gmail.com>
2024-09-24 17:58:32 -0300

    conhecendo o basico

---
+++ src/data/data-geral.ts
@@ -0,0 +1,18 @@
+export const GERAL = [
+  {
+    id: 1,
+    nome: "igor"
+  },
+  {
+    id: 2,
+    nome: "Matheus"
+  },
+  {
+    id: 3,
+    nome: "Vinicius"
+  },
+  {
+    id: 4,
+    nome: "Edmilson"
+  },
+]
