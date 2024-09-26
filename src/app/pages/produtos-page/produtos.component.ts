1d1a41caa4c23f92851c7edc4f7797dc262aede0
IgorMouraS <igormsousa2003@gmail.com>
2024-09-26 18:04:07 -0300

    criando a primeira versão das pages

--- src/app/pages/produtos-page/produtos.component.ts
+++ src/app/pages/produtos-page/produtos.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { ProdutoComponent } from "../../components/produto/produto.component";
 
 @Component({
   selector: 'app-produtos',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ProdutoComponent],
   templateUrl: './produtos.component.html',
   styleUrl: './produtos.component.css'
 })
