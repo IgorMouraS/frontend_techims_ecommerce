74248b8feaa8f05aa81c1bb5cfdfeb723cde9539
Igor Moura Sousa <igormsousa2003@gmail.com>
2024-10-12 10:20:24 -0300

    deixando as informações dinamicas em arquivos de dados

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BOTOES } from '../../../data/data-btn-header';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  navBar = BOTOES;
 }
---
+++ src/data/data-btn-header.ts
@@ -0,0 +1,6 @@
+export const BOTOES = {
+    logo: "TECH IMS",
+    btn_active: "Sobre",
+    btn: ["Carrinho"],
+    btn_search: "Search"
+}
